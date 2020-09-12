0571c801430d4cf904752a7dfa85cb043420165f
Prasanna050399 <56738929+Prasanna050399@users.noreply.github.com>
2020-09-13 00:54:08 +0530

    Insertion, Bubble, Selection sort visualisation
    This commit adds bubble sort to the program. The display method is still based on naive technique meaning we are still updating the graph by clearing its contents and re-plotting the graph.

--- proj_try1.py
+++ proj_try1.py
@@ -11,24 +11,38 @@ def plot(l1,l2,flag1,flag2,index1,index2,name):
         #time.sleep(2)
         temp[index1].set_color('r')
         temp[index2].set_color('r')
-        mp.pause(1)
+        mp.pause(0.2)
         #mp.close()
         mp.clf()
     else:
         mp.show()
-        
+
+def bubblesort(l1,l2,n):
+    name="Bubble Sort"
+    for i in range(n-1):
+        for j in range(0,n-i-1):
+            if l1[j]>l1[j+1]:
+                plot(l2,l1,0,False,j,j+1,name)
+                #plot(12,l1,0,False,j,j+1,name)
+                l1[j],l1[j+1]=l1[j+1],l1[j]
+                #time.sleep(0.1)
+                plot(l2,l1,0,False,j,j+1,name)
+                #print(l2,l1)
+            
+                
 def insertion(l1,l2,n):
     name='Insertion sort'
     for i in range(1, n):
         j=0
-        plot(l2,l1,0,False,i,i,name)
+        #plot(l2,l1,0,False,i,i,name)
         key = l1[i] 
         j = i-1
-        while j >=0 and key < l1[j] : 
+        while j >=0 and key < l1[j] :
+            plot(l2,l1,0,False,i,j,name)
             l1[j+1] = l1[j] 
             j -= 1
         l1[j+1] = key
-        plot(l2,l1,0,False,i,i,name)
+        plot(l2,l1,0,False,i,j,name)
         
 def selection(l1,l2,n):
     name='selection sort'
@@ -73,14 +87,17 @@ def main():
         #plot(l1,l2,0,False,0,0,'Insertion sort')
         insertion(l1,l2,n)
         plot(l1,l2,1,True,0,0,'Insertion sort')
-        
+    def bubble():
+        bubblesort(l1,l2,n)
+        plot(l1,l2,1,True,0,0,'Bubble sort')
+
     d1=dict()
     d1[1]=select
     d1[2]=insert
     #print(d1[1])
     #temp=d1[1]
     #print(type(temp))
-    print("Enter 1 for selection sort\nEnter 2 for insertion sort")
+    print("Enter 1 for selection sort\nEnter 2 for insertion sort\nEnter 3 for Bubble sort")
     ans=int(input())
     
     #d1.get(ans,None)
@@ -88,6 +105,9 @@ def main():
         select()
     elif ans==2:
         insert()
-
+    elif ans==3:
+        bubble()
+    print(l1)
+    
 if __name__ == "__main__":
     main()
