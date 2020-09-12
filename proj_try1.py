7901ebd069174340e8b09119d2d646edccf659fa
Prasanna050399 <56738929+Prasanna050399@users.noreply.github.com>
2020-09-12 23:40:25 +0530

    insertion sort and selection sort visualisation
    1. Selection sort is added in the script.
    2. Menu driven program.
    3. Bar graph is added instead of normal graph.
    4. The bar graph changes during run time. (Dynamic plotting of the values using naive method meaning clearing the graph and re-plotting the entire graph.)
    5. Some doubt about insertion sort.

--- proj_try1.py
+++ proj_try1.py
@@ -1,26 +1,93 @@
 import matplotlib.pyplot as mp
+import time
 import random
 
-def plot(l1,l2):
-    mp.plot(l1,l2)
-    mp.show()
+def plot(l1,l2,flag1,flag2,index1,index2,name):
+    temp=mp.bar(l1,l2)
+    #mp.close()
+    mp.title(name)
+    if flag1==0 and not flag2:
+        mp.show(block=False)
+        #time.sleep(2)
+        temp[index1].set_color('r')
+        temp[index2].set_color('r')
+        mp.pause(1)
+        #mp.close()
+        mp.clf()
+    else:
+        mp.show()
+        
 def insertion(l1,l2,n):
-    for i in range(1, n): 
-        plot(l1,l2)
+    name='Insertion sort'
+    for i in range(1, n):
+        j=0
+        plot(l1,l2,0,False,i,i,name)
         key = l1[i] 
         j = i-1
         while j >=0 and key < l1[j] : 
             l1[j+1] = l1[j] 
             j -= 1
         l1[j+1] = key
+        plot(l1,l2,0,False,i,i,name)
         
+def selection(l1,l2,n):
+    name='selection sort'
+    for i in range(n):
+        min_ind=i
+        for j in range(i+1,n):
+            if l1[min_ind]>l1[j]:
+                min_ind=j
+        plot(l1,l2,0,False,i,min_ind,name)
+        l1[i],l1[min_ind]=l1[min_ind],l1[i]
+        plot(l1,l2,0,False,i,min_ind,name)
 
-l1=list()
-n=100
-for i in range(n):
-    l1.append(random.randrange(n))
 
-l2=list(range(n))
 
-insertion(l1,l2,n)
-plot(l1,l2)
+
+def main():
+    l1=list()
+    n=10
+    d1=dict()
+    i=0
+    while i<n:
+        temp=random.randrange(n)
+        if temp in d1:
+            continue
+        else:
+            d1[temp]=0
+            l1.append(temp)
+            i+=1
+    l2=list(range(n))
+    #l2.append(9)
+    #n+=1
+    #l1.append(10)
+    print(l1)
+    #insertion(l1,l2,n)
+    #selection(l1,l2,n)
+    def select():
+        print("in selection")
+        #plot(l1,l2,0,False,0,0,"selection sort")
+        selection(l1,l2,n)
+        plot(l1,l2,1,True,0,0,'selection sort')
+    def insert():
+        #plot(l1,l2,0,False,0,0,'Insertion sort')
+        insertion(l1,l2,n)
+        plot(l1,l2,1,True,0,0,'Insertion sort')
+        
+    d1=dict()
+    d1[1]=select
+    d1[2]=insert
+    #print(d1[1])
+    #temp=d1[1]
+    #print(type(temp))
+    print("Enter 1 for selection sort\nEnter 2 for insertion sort")
+    ans=int(input())
+    
+    #d1.get(ans,None)
+    if ans==1:
+        select()
+    elif ans==2:
+        insert()
+
+if __name__ == "__main__":
+    main()
