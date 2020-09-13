60b7c0ebf3b9aa5ffec62a03111f3cd8a730d448
Prasanna050399 <56738929+Prasanna050399@users.noreply.github.com>
2020-09-13 22:11:06 +0530

    Quick Sort
    This update contains the visualisation of the quick-sort sorting algorithm. For visualisation I have used matplotlib. The idea for displaying the data is still by using naive approach. Meaning I am still not using the animation method and therefore to update the graph, the graph is cleared and all the elements are re-plotted again.

--- proj_try.py
+++ proj_try.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as mp
+import random
 
-
-def partition(l1,start,end):
+def partition(l1,start,end,l2):
     i=start+1
     pivot=l1[start]
     j=end
     #print(start,end)
+    plot(l2,l1,0,0,start,True)
     while i <= j:
         if l1[i]<pivot:
             i+=1
@@ -21,6 +22,7 @@ def partition(l1,start,end):
             #j-=1
             #print("swapped")
         #print(l1,i,j)
+        plot(l2,l1,i,j,start,True)
     #l1[start],l1[j]=l1[j],l1[start]
     temp=l1[start]
     l1[start]=l1[j]
@@ -29,8 +31,10 @@ def partition(l1,start,end):
     return j
         
 def quick(l1,start,end):
+    #print(l1)
+    l2=list(range(len(l1)))
     if start<end:
-        p=partition(l1,start,end)
+        p=partition(l1,start,end,l2)
         #print(p)d
         #d1=dict()
         #count=0
@@ -38,17 +42,56 @@ def quick(l1,start,end):
         #    d1[count]=i
         #    count+=1
         #plot(d1)
-        l2=[range(len(l1))]
-        plot(l2,l1)
         quick(l1,start,p-1)
         quick(l1,p+1,end)
-def plot(l2,l1):
+        #print(l2)
+        #plot(l1,l2)
+def plot(l2,l1,i,j,pivot,flag):
     #mp.pyplot(list(d1.keys()),list(d1.values(),color="BLACK"))
-    mp.plot(l2,l1)
-    mp.show()
-l1=[1,4,5,2,3,7,41]
-quick(l1,0,len(l1)-1)
+    temp=mp.bar(l2,l1)
+    mp.title("Quick Sort")
+    if flag :
+        if i<len(l2) and j<len(l2):
+            #print(i,j)
+            temp[i].set_color('r')
+            temp[j].set_color('r')
+            temp[pivot].set_color('g')
+            mp.title("Quick Sort")
+            mp.show(block=False)
+            mp.pause(0.2)
+            mp.clf()
+        else:
+            mp.show(block=False)
+            mp.pause(0.2)
+            mp.clf()
+    else:
+        
+        mp.show()
+#l1=[1,4,5,2,3,7,41]
+l1=list()
+n=20
+#for i in range(20):
+    #    l1.append(random.ra)%100)
+#    l1.append(random.randrange(100))
+#l1=[76, 35, 60, 88, 60, 32, 67, 75, 73, 66, 77, 93, 33, 0, 57, 74, 75, 7, 10, 77]
+i=0
+d1=dict()
+while i<n:
+    temp=random.randrange(n)
+    if temp in d1:
+        continue
+    else:
+        d1[temp]=0
+        l1.append(temp)
+        i+=1
 print(l1)
-l2=[10,80,30,90,40,50,70]
-quick(l2,0,len(l2)-1)
-print(l2)
+quick(l1,0,len(l1)-1)
+l2=list(range(20))
+print(l1)
+plot(l2,l1,0,0,0,False)
+
+#print(l1)
+#l2=[10,80,30,90,40,50,70]
+#quick(l2,0,len(l2)-1)
+#print(l2)
+#[76, 35, 60, 88, 60, 32, 67, 75, 73, 66, 77, 93, 33, 0, 57, 74, 75, 7, 10, 77]
